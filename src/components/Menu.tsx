161c576bbf2b690e65e3c63f4e37b497f5b03556
marianoariassoy <hola@marianoarias.soy>
2023-07-04 19:23:04 -0300

    add reserva

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,7 +6,12 @@ const Menu = () => {
 
   return (
     <div className="flex gap-8 items-center">
-      <a href="http://" target="_blank" className="border-2 border-white py-2 px-6 hover:bg-white hover:text-black transition-all hidden lg:block" rel="noopener noreferrer">
+      <a
+        href="https://palmaresnaturehousing.reservadirecto.com/lp.html?pos=PalmaresNatureHousing&lng=es&cur=ARS&webcache=off&tag=PmsLink&appID=111"
+        target="_blank"
+        className="border-2 border-white py-2 px-6 hover:bg-white hover:text-black transition-all hidden lg:block"
+        rel="noopener noreferrer"
+      >
         Reservas
       </a>
       <div id="nav-menu" onClick={OpenMenu}>
--- src/components/WaComponent.tsx
+++ src/components/WaComponent.tsx
@@ -2,7 +2,7 @@ import { Whatsapp } from "../icons/svgs";
 
 const WaComponent = () => {
   return (
-    <a href="https://wa.me/#" target="_blank" rel="noreferrer" className="fixed z-20 bottom-8 right-8 drop-shadow text-primary">
+    <a href="https://wa.me/5493874081077" target="_blank" rel="noreferrer" className="fixed z-20 bottom-8 right-8 drop-shadow text-primary">
       <Whatsapp />
     </a>
   );
